ad11a7b6f69ed21feb2442eb61757001475296c7
Guilherme Guimaraes <Technestowl@gmail.com>
2024-10-24 13:32:26 -0400

    added notes in index.php on how the logic is working

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         <!-- To-Do List -->
         <ul id="todoList">
             <?php
-                // read tasks from the file and display them
+                // read tasks from the file and output them to this UL. 
                  $tasksFile = 'tasks.txt';
                  if(file_exists($tasksFile)){
                     $tasks = file($tasksFile, FILE_IGNORE_NEW_LINES);
@@ -32,6 +32,7 @@
                             "<a href='task-handler.php?delete=$index'>Delete</a></li>";
                     }
                  }
+                 //tasks are being loaded from the server each time the page is refreshed.
             ?>
         </ul>
     </div>
@@ -39,4 +40,12 @@
     <!-- Link to JavaScript -->
     <script src="script.js"></script>
 </body>
+<!-- 
+   User adds a task: User types in a task and clicks "add task" btn. This sends a POST request
+   to task-handler.php, which appends said task to tasks.txt and redirects back to index.php to display updated task list.
+
+   User deletes a task: User clicks "delete" which sends a GET request to task-handler.php with said tasks index #. The script 
+   removes the task from the file and reloads the page with updated the updated list.
+ -->
 </html>
+
